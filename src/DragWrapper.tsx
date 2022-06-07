6b49f769e759683877d4c08965484cb496a28b66
Eneko96 <imr.aem2526@gmail.com>
2022-06-07 11:48:20 +0200

    updated dragging events

--- src/DragWrapper.tsx
+++ src/DragWrapper.tsx
@@ -13,39 +13,17 @@ import React, { MouseEvent, useRef, useState } from "react";
 }
 
 interface IDraggable {
-  onDrag: (e: MouseEvent<any>) => any,
-  onDragEnd: (e: MouseEvent<any>, ...args: any) => any,
-  onDragStart?: (e: MouseEvent<any>, ...args: any) => any,
   ref: React.Ref<any>
   children: React.ReactNode
   id: string
 }
 
-const Draggable: React.FC<IDraggable> = React.forwardRef(({ children, onDrag, onDragEnd, onDragStart, id }, ref) => {
-  const [dragging, setDragging] = useState(false);
-
-  const handleDragStart = (e:any) => {
-    setDragging(true);
-    onDragStart(e);
-  };
-  const handleDrag = (e:any) => {
-    if (dragging) {
-      onDrag(e);
-    }
-  };
-  const handleDragEnd = (e:any) => {
-    console.log("drag end", e);
-    setDragging(false);
-    onDragEnd(e);
-  };
+const Draggable: React.FC<IDraggable> = React.forwardRef(({ children, id }, ref) => {
   return (
     <div
       id={id}
-      style={{ position: 'relative', backgroundColor: 'red', width: '100%', height: '3rem', margin: '1rem' }}
+      style={{ position: 'relative', backgroundColor: 'red', padding: '1rem', height: '3rem', margin: '1rem', display: 'inline-block' }}
       ref={ref}
-      onMouseDown={handleDragStart}
-      onMouseMove={handleDrag}
-      onMouseUp={handleDragEnd}
     >
       {children}
     </div>
@@ -53,26 +31,65 @@ const Draggable: React.FC<IDraggable> = React.forwardRef(({ children, onDrag, on
 })
 
 export const DragWrapper: React.FC<any> = ({ children }) => {
-  const childRef = useRef<HTMLElement[]>([])
-  const setChildRef = (index:number) => (el:React.ReactElement) => childRef[index] = el as React.ReactElement
+  const [isDragging, setIsDragging] = useState<boolean>(false)
+  const [dragElement, setDragElement] = useState<number|null>(null)
+  const childRef = useRef(null)
+  const parentRef = useRef(null);
+  const setChildRef = (index:number) => (el:React.ReactElement) => childRef[index] = el
 
-  const handleElements = (e: MouseEvent<HTMLDivElement>,ind:number) => {
-    console.log(childRef[0], ind)
-    const element: HTMLDivElement = childRef[ind]
-    const elementPosition = element.getBoundingClientRect()
-    const { x, y } = elementPosition
-    console.log(x, y)
+  const handleMouseDown = (e: MouseEvent<HTMLElement>) => {
+    console.log(e)
+    console.warn('mouse down')
+    const inElement = () => {
+      console.log(childRef, Object.keys(childRef))
+      for (let i = 0; i < Object.keys(childRef).length-1; i++) {
+        const element = childRef[i] as HTMLElement
+        const elementPosition = element.getBoundingClientRect()
+        const { clientX: x, clientY: y } = e
+        const { bottom: parentBottom, top: parentTop, left: parentLeft, right: parentRight } = elementPosition
+        if (x > parentLeft && x < parentRight && y > parentTop && y < parentBottom) {
+          console.log(x, y)
+          console.log('in element')
+          setDragElement(i)
+          setIsDragging(true)
+        }
+      }
+    }
+    inElement()
+  }
+
+  const handleMouseMove = (e: MouseEvent<HTMLElement>) => {
+    if (isDragging) {
+      console.warn('moving')
+      const { clientX: x, clientY: y } = e
+      // console.log('antique position', )
+      console.log('actual position', x, y)
+    }
+  }
+
+  const handleMouseUp = (e: MouseEvent<HTMLElement>) => {
+    console.warn('mouse up')
+    if (isDragging) {
+      console.log(childRef[dragElement].offsetWidth)
+      childRef[dragElement].style.left = e.clientX - childRef[dragElement].offsetWidth / 2 +'px'
+      childRef[dragElement].style.top = e.clientY+'px'
+    }
+    setIsDragging(false)
   }
 
   return (
-    <div className="drag-container">
+    <div
+      className="drag-container"
+      ref={parentRef}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      style={{ backgroundColor: '#00f0ff'}}
+    >
       {children.map((child:any, index:any) => (
         <Draggable
           ref={setChildRef(index)}
           id={index}
-          onDrag={() => console.log('hi')}
-          onDragEnd={(e:MouseEvent<HTMLDivElement>) => handleElements(e,index)}
-          onDragStart={() => console.log('nothing at the moment')}
           key={index+75}>{child}
         </Draggable>
         ))}
