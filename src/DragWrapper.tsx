57c117f4dbb41178d3c49eb97bbc9d59e5491840
Eneko96 <imr.aem2526@gmail.com>
2022-06-07 20:57:01 +0200

    added transition effects and touch events

--- src/DragWrapper.tsx
+++ src/DragWrapper.tsx
@@ -21,8 +21,8 @@ interface IDraggable {
 const Draggable: React.FC<IDraggable> = React.forwardRef(({ children, id }, ref) => {
   return (
     <div
+      className="draggable"
       id={id}
-      style={{ position: 'relative', backgroundColor: 'red', padding: '1rem', height: '3rem', margin: '1rem', display: 'inline-block' }}
       ref={ref}
     >
       {children}
@@ -61,18 +61,37 @@ export const DragWrapper: React.FC<any> = ({ children }) => {
   const handleMouseMove = (e: MouseEvent<HTMLElement>) => {
     if (isDragging) {
       console.warn('moving')
-      const { clientX: x, clientY: y } = e
-      // console.log('antique position', )
-      console.log('actual position', x, y)
+      const element = childRef[dragElement]
+      element.style.left = e.clientX - element.offsetWidth +'px'
+      element.style.top = e.clientY - element.offsetHeight +'px'
+      const isTouching = Object.entries(childRef).find(([key, value]) => {
+        if (key !== 'current' && key !== (dragElement as number).toString()) {
+          const elementPosition = value.getBoundingClientRect() as DOMRect
+          const { bottom: parentBottom, top: parentTop, left: parentLeft, right: parentRight } = elementPosition
+          const { clientX: x, clientY: y } = e
+          return x > parentLeft && x < parentRight && y > parentTop && y < parentBottom
+        }
+      })
+      if (isTouching?.length > 0) {
+        console.log('touching')
+        const [,value] = isTouching
+        value.style.left = '0px'
+        value.style.top = '0px'
+        value.style.transition = 'left 0.2s, top 0.2s';
+        value.addEventListener('transitionend', () => {
+          value.style.transition = ''
+        })
+      }
     }
   }
 
   const handleMouseUp = (e: MouseEvent<HTMLElement>) => {
     console.warn('mouse up')
     if (isDragging) {
-      console.log(childRef[dragElement].offsetWidth)
-      childRef[dragElement].style.left = e.clientX - childRef[dragElement].offsetWidth / 2 +'px'
-      childRef[dragElement].style.top = e.clientY+'px'
+      const element = childRef[dragElement]
+      console.log(element.style.left, element.offsetWidth)
+      element.style.left = e.clientX - element.clientWidth +'px'
+      element.style.top = e.clientY - element.clientHeight +'px'
     }
     setIsDragging(false)
   }
